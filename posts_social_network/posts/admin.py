c26abbddb298fbea82d2e9032278a78ed012f412
Vladislav <vladislav.sytyuk@gmail.com>
2022-01-17 18:31:36 +0200

    Register model UserActivity

--- posts_social_network/posts/admin.py
+++ posts_social_network/posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Like, Post
+from .models import Like, Post, UserActivity
 
 
 admin.site.register(Like)
 admin.site.register(Post)
+admin.site.register(UserActivity)
