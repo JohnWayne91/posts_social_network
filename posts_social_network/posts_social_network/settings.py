072116d63cf1a20d014ac5f4ecd1ad8acf6b611b
Vladislav <vladislav.sytyuk@gmail.com>
2022-01-17 18:36:47 +0200

    Register middleware for tracking last user activity

--- posts_social_network/posts_social_network/settings.py
+++ posts_social_network/posts_social_network/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'posts.middleware.UpdateLastActivityMiddleware'
 ]
 
 ROOT_URLCONF = 'posts_social_network.urls'
