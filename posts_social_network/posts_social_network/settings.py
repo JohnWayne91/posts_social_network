a1bb03a072699a59af1256def2c9c0a235dea85a
Vladislav <vladislav.sytyuk@gmail.com>
2022-01-19 10:40:36 +0200

    Add rest_framework_simplejwt to DEFAULT_AUTHENTICATION_CLASSES

--- posts_social_network/posts_social_network/settings.py
+++ posts_social_network/posts_social_network/settings.py
@@ -133,7 +133,7 @@ AUTH_USER_EMAIL_UNIQUE = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
