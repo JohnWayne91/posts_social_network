e5153ad7e52265ef3f3d2f6fc66a534396f6a67a
Vladislav <vladislav.sytyuk@gmail.com>
2022-01-24 17:27:30 +0200

    Change order of install apps

--- posts_social_network/posts_social_network/settings.py
+++ posts_social_network/posts_social_network/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts',
     'rest_framework',
     'rest_framework_simplejwt',
-    'django_filters'
+    'django_filters',
+    'posts'
 ]
 
 MIDDLEWARE = [
